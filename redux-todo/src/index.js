399ccd343634421bb4300212f2215bfdfcfb1760
blakelower <blakelower1999@gmail.com>
2020-02-18 20:57:45 +0700

    added action types

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// STEP-1 DESIGN APPLICATION STATE
+// {
+//   formValues: {       // SLICE 1
+//     fname: '',
+//     lname: '',
+//   },
+//   friends: [          // SLICE 2
+//     { id: someId, fname: 'Jane', lname: 'Doe', married: false },
+//     { id: someId, fname: 'Jane', lname: 'Doe', married: false },
+//     { id: anotherId, fname: 'john', lname: 'Smith', married: false },
+//   ],
+//   numberOfFriends: 3, // SLICE 3 BAD BAD BAD BAD!!!!!!!! this can be computed from slice 2
+// }
+const combinedReducer = combineReducers({
+    formValues: formReducer,
+    friends: friendsReducer,
+  })
+
+
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
